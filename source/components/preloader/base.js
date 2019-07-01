4ca609e03a7e6f07d44c0d5729e9c59a65d2c432
pramayu <gedepramayu@gmail.com>
2019-07-01 22:01:28 +0800

    transition animation & react navigation install

--- source/components/preloader/base.js
+++ source/components/preloader/base.js
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
 import {
   Text, View, TouchableOpacity,
-  StatusBar
+  StatusBar, Animated,
+  Easing
 } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {
   common
 } from '../../assets/stylesheets/common';
 
 
 class BasePreloader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opaciti: new Animated.Value(0)
+    }
+    this.logo = new Animated.Value(0);
+  }
+
+  componentDidMount = () => {
+    this.logoToTop();
+  }
+
+  logoToTop = () => {
+    Animated.parallel([
+      Animated.timing(this.logo, {
+        toValue: 3,
+        duration: 900,
+        useNativeDriver: true,
+      }),
+      Animated.timing(this.state.opaciti, {
+        toValue: 1,
+        duration: 300,
+        delay: 850,
+        useNativeDriver: true,
+      })
+    ]).start()
+  }
+
   render () {
+    var logoToTopSty = this.logo.interpolate({
+      inputRange: [0, 1, 2, 3],
+      outputRange: [200, -20, 5, 0]
+    });
     return (
       <View style={[common.container, { backgroundColor: '#fe9d07' }]}>
         <StatusBar backgroundColor="#fe9d07" barStyle="light-content" />
-        <View style={{flex: .4, justifyContent: 'flex-end', alignItems: 'center'}}>
-          <MaterialIcons name="landscape" color="#fdf4ec" size={60}/>
-          <Text style={{transform: [{translateY: -20}],margin: 0,fontSize: 18, color: '#fdf4ec', letterSpacing: 3, fontFamily: 'Oswald'}}>POCENI</Text>
+        <View style={{flex: .5, justifyContent: 'flex-end', alignItems: 'center'}}>
+          <Animated.View style={{width: 150, height: 150, justifyContent: 'center', alignItems: 'center', opacity: 1, transform:[{translateY: logoToTopSty}]}}>
+            <MaterialIcons name="landscape" color="#fdf4ec" size={72}/>
+            <Text style={{transform: [{translateY: -20}],margin: 0,fontSize: 22, color: '#fdf4ec', letterSpacing: 3, fontFamily: 'Oswald'}}>POCENI</Text>
+          </Animated.View>
         </View>
-        <View style={{flex: .1}}></View>
-        <View style={{flex: .5, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{flex: .2, justifyContent: 'flex-end', alignItems: 'center'}}>
+        </View>
+        <Animated.View style={{flex: .4, justifyContent: 'center', alignItems: 'center', opacity: this.state.opaciti}}>
           <TouchableOpacity style={{width: '50%', height: 35, borderRadius: 20, backgroundColor: '#fdf4ec', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={[common.fontitle, {fontSize: 13, color: '#fe9d07'}]}>Make Account</Text>
           </TouchableOpacity>
           <TouchableOpacity style={{marginTop: 20, width: '50%', height: 34, borderRadius: 20, backgroundColor: '#fe9d07', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={[common.fontitle, {fontSize: 13, color: '#fdf4ec'}]}>Let's Login</Text>
           </TouchableOpacity>
-        </View>
+        </Animated.View>
       </View>
     )
   }
